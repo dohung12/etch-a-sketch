3deaabf439bc3824fb9358c824475419fc83d049
dohung <dohuyhung12@gmail.com>
2021-12-12 21:08:35 +0700

    drawing color btn event listener

--- app.js
+++ app.js
@@ -2,11 +2,26 @@
 let matrixSize = 16;
 const tilesMatrix = document.querySelector(".tile-matrix");
 const resetBtn = document.querySelector(".reset-btn");
+const toggleColorBtn = document.querySelector(".toggle-color");
+
+let blackAndWhiteFlag = true;
 //EVENT LISTENER
 document.addEventListener("DOMContentLoaded", createDivMatrix(16));
 
 resetBtn.addEventListener("click", resetTilesMatrix);
+
+toggleColorBtn.addEventListener("click", switchDrawingColor);
 // FUNCTIONS
+
+function switchDrawingColor() {
+  if (blackAndWhiteFlag) {
+    blackAndWhiteFlag = false;
+    toggleColorBtn.textContent = "Color mode";
+  } else {
+    blackAndWhiteFlag = true;
+    toggleColorBtn.textContent = "Black and white mode";
+  }
+}
 
 function resetTilesMatrix(e) {
   const tiles = document.querySelectorAll(".tile");
@@ -39,9 +54,10 @@ function createDiv(className) {
 
 function hoverTile(e) {
   let bgColor = e.currentTarget.style.backgroundColor;
-  if (bgColor === "") {
+  if (bgColor === "" && !blackAndWhiteFlag) {
     e.currentTarget.style.backgroundColor = randomHSL();
-    console.log(e.currentTarget.style.backgroundColor);
+  } else if (bgColor === "" && blackAndWhiteFlag) {
+    e.currentTarget.style.backgroundColor = "black";
   }
 }
 
